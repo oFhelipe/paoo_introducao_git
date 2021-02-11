30194b4091ee97e8ecb8cd03828cd482de132c78
Fhelipe Rodrigues <fhelipe.rodrigues@fatec.sp.gov.br>
2021-02-11 15:32:45 -0300

    Novo commit depois do novo add

--- EntregarGit.java
+++ EntregarGit.java
@@ -1,5 +1,7 @@
 public class EntregaGit{
     public static void main(String[] args){
+        String nome = JOptionPane.showInputDialog("Qual o seu nome?");
+        JOptionPane.showMessageDialog(null, "Bem vindo " + nome);
         System.out.println("Entrando tarefas com o Git na linha de comando");
     }
 }
